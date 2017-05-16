e85311441e274d3296e591f6a843548f749dce32
zzgit00 <1109440800@qq.com>
2017-05-16 09:32:13 +0800

    策略模式

---
+++ StrategyModel/StrategyModel/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace StrategyModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text_price = new System.Windows.Forms.TextBox();
+            this.text_num = new System.Windows.Forms.TextBox();
+            this.calculate_method = new System.Windows.Forms.ComboBox();
+            this.ok = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.goodsList = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "单　　价";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "数　　量";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "计算方式";
+            // 
+            // text_price
+            // 
+            this.text_price.Location = new System.Drawing.Point(71, 6);
+            this.text_price.Name = "text_price";
+            this.text_price.Size = new System.Drawing.Size(171, 21);
+            this.text_price.TabIndex = 3;
+            // 
+            // text_num
+            // 
+            this.text_num.Location = new System.Drawing.Point(71, 32);
+            this.text_num.Name = "text_num";
+            this.text_num.Size = new System.Drawing.Size(171, 21);
+            this.text_num.TabIndex = 4;
+            // 
+            // calculate_method
+            // 
+            this.calculate_method.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.calculate_method.FormattingEnabled = true;
+            this.calculate_method.Location = new System.Drawing.Point(71, 58);
+            this.calculate_method.Name = "calculate_method";
+            this.calculate_method.Size = new System.Drawing.Size(171, 20);
+            this.calculate_method.TabIndex = 5;
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(248, 4);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(75, 23);
+            this.ok.TabIndex = 6;
+            this.ok.Text = "确　　定";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(248, 30);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 7;
+            this.reset.Text = "重　　置";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // goodsList
+            // 
+            this.goodsList.FormattingEnabled = true;
+            this.goodsList.ItemHeight = 12;
+            this.goodsList.Location = new System.Drawing.Point(12, 84);
+            this.goodsList.Name = "goodsList";
+            this.goodsList.Size = new System.Drawing.Size(311, 88);
+            this.goodsList.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "总　　计";
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.Font = new System.Drawing.Font("宋体", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_result.Location = new System.Drawing.Point(71, 175);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(0, 67);
+            this.label_result.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 241);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.goodsList);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.calculate_method);
+            this.Controls.Add(this.text_num);
+            this.Controls.Add(this.text_price);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "商城收银软件";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text_price;
+        private System.Windows.Forms.TextBox text_num;
+        private System.Windows.Forms.ComboBox calculate_method;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.ListBox goodsList;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label_result;
+    }
+}
+
---
+++ StrategyModel/StrategyModel/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StrategyModel
+{
+    public partial class Form1 : Form
+    {
+        double total = 0.0d;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            CashContext csuper = new CashContext(calculate_method.SelectedItem.ToString());
+            double totalPrices = 0d;
+            totalPrices = csuper.GetResult(Convert.ToDouble(text_price.Text) * Convert.ToDouble(text_num.Text));
+            total += totalPrices;
+
+            goodsList.Items.Add("单价：" + text_price.Text + " 数量：" + text_num.Text + " 合计：" + totalPrices);
+            label_result.Text = total.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            calculate_method.Items.Add("正常收费");
+            calculate_method.Items.Add("满 300 返 100");
+            calculate_method.Items.Add("打 8 折");
+            calculate_method.SelectedIndex = 0;
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            goodsList.Items.Clear();
+            label_result.Text = (total = 0).ToString();
+        }
+    }
+
+    public abstract class CashSuper
+    {
+        public abstract double acceptCash(double money);
+    }
+
+    public class CashNormal : CashSuper
+    {
+        public override double acceptCash(double money)
+        {
+            return money;
+        }
+    }
+
+    public class CashRebate : CashSuper
+    {
+        private double moneyRebate = 1d;
+
+        public CashRebate(String moneyRebate)
+        {
+            this.moneyRebate = double.Parse(moneyRebate);
+        }
+
+        public override double acceptCash(double money)
+        {
+            return money * moneyRebate;
+        }
+    }
+
+    public class CashReturn : CashSuper
+    {
+        private double moneyCondition = 0.0d;
+        private double moneyReturn = 0.0d;
+
+        public CashReturn(String moneyCondition, String moneyReturn)
+        {
+            this.moneyCondition = double.Parse(moneyCondition);
+            this.moneyReturn = double.Parse(moneyReturn);
+        }
+
+        public override double acceptCash(double money)
+        {
+            return 0d;
+        }
+    }
+
+    public class CashContext
+    {
+        private CashSuper cs = null;
+
+        public CashContext(String type)
+        {
+            switch (type)
+            {
+                case "正常收费":
+                    cs = new CashNormal();
+                    break;
+                case "满 300 返 100":
+                    cs = new CashReturn("300", "100");
+                    break;
+                case "打 8 折":
+                    cs = new CashRebate("0.8");
+                    break;
+            }
+        }
+
+        public double GetResult(double money)
+        {
+            return cs.acceptCash(money);
+        }
+    }
+
+}
